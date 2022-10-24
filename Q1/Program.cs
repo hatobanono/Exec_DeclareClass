4f168881f0cbfb7db93ca336962511eb34ef56bd
hatobanono <chris00iq@gmail.com>
2022-10-24 17:45:15 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Address
+	{
+		public string City { get; set; }
+		public string ZipCode { get; set; }
+		public string Street { get; set; }
+	}
+
+	class Telephone
+	{
+		public string CountryCode { get; set; }
+		public string AreaCode { get; set; }
+		public string Number { get; set; }
+		public string Ext { get; set; }
+	}
+
+	class Member
+	{
+		public string Name { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+		public string Cellphone { get; set; }
+		public Address Addr { get; set; }
+		public Telephone Tele { get; set; }
+
+		public void Register(string name,
+							 string account,
+							 string password,
+							 string confirmPassword,
+							 string email)
+		{
+
+		}
+
+		public void ForgetPassword(string name, string email)
+		{
+
+		}
+
+		public void Authenticate(string account, string password)
+		{
+
+		}
+
+	}
+
+}
